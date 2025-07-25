171453f05a88b7d3a3b0845682febbbd71cab23e
memoryleak47 <memoryleak47@protonmail.ch>
2025-07-25 16:54:27 +0100

    ...

--- src/strategy/run.rs
+++ src/strategy/run.rs
@@ -2,12 +2,16 @@ use crate::*;
 
 pub type ValueId = usize;
 
-
 // TODO: open question:
 // Should sigma only map pvars from main, or generally any variable from context?
 pub type Sigma = Map<String, ValueId>;
 pub type Deref = Map<ValueId, RecExpr<Semi>>;
 
 pub fn eval(expr: &Expr, ast: &Ast, sigma: Sigma, deref: Deref, eg: &EGraph<GeneralLang, ()>) -> Vec<(Sigma, Deref, RecExpr<Semi>)> {
-    todo!()
+    match expr {
+        Expr::Match(m) => todo!(),
+        Expr::DataConstr(s, args) => todo!(),
+        Expr::FnCall(f, args) => todo!(),
+        Expr::Var(s) => todo!(),
+    }
 }
