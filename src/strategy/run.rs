9eb4f90a2e45b783cb35bf199f5bee7d0482f68d
memoryleak47 <memoryleak47@protonmail.ch>
2025-07-25 17:58:07 +0100

    ...

--- src/strategy/run.rs
+++ src/strategy/run.rs
@@ -5,23 +5,29 @@ pub type ValueId = Id;
 pub type Sigma = Map<String, ValueId>; // same as var_ctxt in previous impl.
 pub type Deref = Map<ValueId, Semi>; // Children of Semi are ValueIds again.
 
+fn eval_args(args: &[Expr], ast: &Ast, sigma: Sigma, deref: Deref, eg: &EGraph<GeneralLang, ()>) -> Vec<(Deref, Vec<ValueId>)> {
+    let mut states: Vec<(Deref, /*args*/Vec<ValueId>)> = vec![(deref, Vec::new())];
+    for a in args.iter() {
+        states = states.into_iter().map(|(deref, args)| {
+            eg_eval(a, ast, sigma.clone(), deref, eg)
+                .into_iter()
+                .map(move |(deref, a)| {
+                    let mut args = args.clone();
+                    args.push(a);
+                    (deref, args)
+                })
+        }).flatten().collect();
+    }
+    states
+}
+
 pub fn eg_eval(expr: &Expr, ast: &Ast, sigma: Sigma, deref: Deref, eg: &EGraph<GeneralLang, ()>) -> Vec<(Deref, ValueId)> {
     match expr {
         Expr::Match(m) => todo!(),
         Expr::DataConstr(s, args) => todo!(),
         Expr::FnCall(f, args) => {
-            let mut states: Vec<(Deref, /*args*/Vec<ValueId>)> = vec![(deref, Vec::new())];
-            for a in args.iter() {
-                states = states.into_iter().map(|(deref, args)| {
-                    eg_eval(a, ast, sigma.clone(), deref, eg)
-                        .into_iter()
-                        .map(move |(deref, a)| {
-                            let mut args = args.clone();
-                            args.push(a);
-                            (deref, args)
-                        })
-                }).flatten().collect();
-            }
+            let states = eval_args(args, ast, sigma, deref, eg);
+
             let mut outs = Vec::new();
             for (deref, args) in states {
                 outs.extend(eg_call_fn(f, &args, deref, ast, eg));
