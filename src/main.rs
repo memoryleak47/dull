2af202db22e023c8d4948610dd84347478ed4015
memoryleak47 <memoryleak47@protonmail.ch>
2025-07-25 19:32:56 +0100

    prepare to run first dull strategy experiment

--- src/main.rs
+++ src/main.rs
@@ -15,8 +15,9 @@ pub use strategy::*;
 
 use std::fs::File;
 use std::io::Read;
-pub use egg::{Language, define_language, Id, Symbol, Rewrite, Searcher, Applier, EGraph, SearchMatches, Var, Subst, PatternAst, RecExpr, SymbolLang};
+pub use egg::{Language, define_language, Id, Symbol, Rewrite, Searcher, Applier, EGraph, SearchMatches, Var, Subst, PatternAst, RecExpr, SymbolLang, Runner};
 use std::collections::HashMap as Map;
+use std::str::FromStr;
 
 fn load_file(filename: &str) -> String {
     let mut file = File::open(filename).expect("Cannot find file!");
@@ -26,10 +27,20 @@ fn load_file(filename: &str) -> String {
     contents
 }
 
-fn main() {
-    let filename = std::env::args().nth(1).unwrap_or(String::from("file.dull"));
+fn dull_rewrite(filename: &str) -> Rewrite<SymbolLang, ()> {
     let s = load_file(&filename);
     let tks = tokenize(s);
     let ast = assemble(&tks);
-    run(&ast);
+    mk_dull_rewrite(ast)
+}
+
+fn main() {
+    let rw = dull_rewrite("rw1.dull");
+    // let j = g.add_term(RecExpr::parse("(Suc foo)").unwrap());
+    let mut runner: Runner<SymbolLang, ()> = Runner::new(());
+    let i = runner.egraph.add_expr(&RecExpr::from_str("foo").unwrap());
+    let j = runner.egraph.add_expr(&RecExpr::from_str("(Suc foo)").unwrap());
+    let runner = runner.run(&[rw]);
+    dbg!(runner.egraph.find(i));
+    dbg!(runner.egraph.find(j));
 }
