0c104ca7d6c3a7470e595f7d31539a497769a948
memoryleak47 <memoryleak47@protonmail.ch>
2025-07-25 17:55:37 +0100

    handle Expr::FnCall

--- src/strategy/run.rs
+++ src/strategy/run.rs
@@ -9,7 +9,25 @@ pub fn eg_eval(expr: &Expr, ast: &Ast, sigma: Sigma, deref: Deref, eg: &EGraph<G
     match expr {
         Expr::Match(m) => todo!(),
         Expr::DataConstr(s, args) => todo!(),
-        Expr::FnCall(f, args) => todo!(),
+        Expr::FnCall(f, args) => {
+            let mut states: Vec<(Deref, /*args*/Vec<ValueId>)> = vec![(deref, Vec::new())];
+            for a in args.iter() {
+                states = states.into_iter().map(|(deref, args)| {
+                    eg_eval(a, ast, sigma.clone(), deref, eg)
+                        .into_iter()
+                        .map(move |(deref, a)| {
+                            let mut args = args.clone();
+                            args.push(a);
+                            (deref, args)
+                        })
+                }).flatten().collect();
+            }
+            let mut outs = Vec::new();
+            for (deref, args) in states {
+                outs.extend(eg_call_fn(f, &args, deref, ast, eg));
+            }
+            outs
+        },
         Expr::Var(s) => vec![(deref, sigma[s])]
     }
 }
